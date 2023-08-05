8b5153ebadc7f07515c8ec1d462e3ade57b83691
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-08-05 19:24:47 +0200

    make table func

---
+++ latexutils/make_table.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def make_table(columns_name, data, caption="table_caption", label="table_label"):
+    '''
+        Create a string with plain latex file with a table using columns name and 2D array data.
+
+        Keyword arguments:
+        columns_name -- list of strings containing table columns name
+        data -- 2D ndarray containing data used to fill the table
+        caption -- string for the caption of LaTeX table (default: "table_caption")
+        label -- string for the label of LaTeX table (default: "table_label")
+    '''
+
+    assert data.shape[1] == len(columns_name), "Error Message: mismatch between number of columns and shape of data"
+
+    # LaTeX preamble
+    p  = "\\documentclass[11pt]{article}\n"
+    p += "\\usepackage{booktabs}\n\n"
+    p += "\\begin{document}\n\n"
+
+    # Table
+    p += "\\begin{table}[!ht]\n"
+    p += "\t\\centering\n"
+    p += "\t\\caption{"+str(caption)+"}\\label{tab:"+label+"}\n"
+    p += "\t\\begin{tabular}{" + "".join([char*len(columns_name) for char in "c"]) + '}\n'
+    p += "\t\t\\toprule\n"
+
+    # Columns name
+    l = "\t\t"
+    for i in range(len(columns_name)):
+        l+= "{:<5s}{:<5s}{:<5s}{}".format("", str(columns_name[i]), "", "&")
+    l = l[:-1]
+    p += l + "\\\\\n"
+    p += "\t\t\\midrule\n"
+
+    # Data
+    for i in range(data.shape[0]):
+        l = "\t\t"
+        for j in range(data.shape[1]):
+            l+= "{:<5s}{:<5s}{:<5s}{}".format("", str(data[i,j]), "", "&")
+        l = l[:-1]
+
+        p += l + "\\\\\n"
+    p += "\t\t\\bottomrule\n"
+    p += "\t\\end{tabular}\n"
+    p += "\\end{table}\n\n"
+
+    # End document
+    p += "\\end{document}\n"
+
+    return p
