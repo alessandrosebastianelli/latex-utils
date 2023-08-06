2df4aeaf54d341a7634687fec9344ad65ac99556
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-08-06 11:33:34 +0200

    better test for make figure

--- tests/test_make_figure.py
+++ tests/test_make_figure.py
@@ -3,17 +3,27 @@ sys.path += ['.']
 
 from latexutils.make_figure import make_image, make_tabular_image
 import numpy as np
+import os
 
 if __name__ == '__main__':
 
 
-    latex_image = make_image('image.png', caption='My image', label='img1', preable=False)
+    latex_image = make_image('fig1.png', caption='My image', label='img1', preable=True)
     print(latex_image)
 
 
     images = np.array(
-            [["fig1.png"],["fig2.png"]]
+            [["fig1.png", "fig2.png"],["fig1.png", "fig2.png"]]
         )
 
-    latex_images = make_tabular_image(images, caption='My image', label='img1', preable=False)
+    latex_images = make_tabular_image(images, caption='My image', label='img1', preable=True)
     print(latex_images)
+
+    save_folder = os.path.join('tmp', 'test_make_image')
+    os.makedirs(save_folder, exist_ok=True)
+    
+    with open(os.path.join(save_folder, 'main-1.tex'), 'w') as texfile:
+        texfile.writelines(latex_image)
+
+    with open(os.path.join(save_folder, 'main-2.tex'), 'w') as texfile:
+        texfile.writelines(latex_images)
